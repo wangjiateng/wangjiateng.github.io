dcf7240a2c99ebee3a262edadb1ab60babb0304c
王佳腾 <641581566@qq.com>
2021-06-23 07:03:08 +0800

    build

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-    // let menuBar = document.getElementById("menu-bar");
-    // let menuTitles = menuBar.getElementsByClassName("menu-title");
-    // for (let i = 0; i < menuTitles.length; i++) {
-    //     let ele = menuTitles[i];
-    //     ele.parentElement.removeChild(ele);  
-    // }
-    // let rightButtons = menuBar.getElementsByClassName("right-buttons");
-    // for (let i = 0; i < rightButtons.length; i++) {
-    //     let ele = rightButtons[i];
-    //     ele.parentElement.removeChild(ele);
-    // }
-    // document.getElementById('theme-toggle').remove();
+    let menuBar = document.getElementById("menu-bar");
+    let menuTitles = menuBar.getElementsByClassName("menu-title");
+    for (let i = 0; i < menuTitles.length; i++) {
+        let ele = menuTitles[i];
+        ele.parentElement.removeChild(ele);  
+    }
+    let rightButtons = menuBar.getElementsByClassName("right-buttons");
+    for (let i = 0; i < rightButtons.length; i++) {
+        let ele = rightButtons[i];
+        ele.parentElement.removeChild(ele);
+    }
+    document.getElementById('theme-toggle').remove();
 };
